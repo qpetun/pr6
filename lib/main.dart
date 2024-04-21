780fd02f79b9c2e722353d54793ef7e9c237b610
Миша <petuninmisha@mail.ru>
2024-04-21 18:51:19 +0700

    Комментарии

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
+// MyApp - корневой виджет приложения
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Пример',
+      title: 'Navigation Пример', // Заголовок приложения
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // Основной цвет темы
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white, backgroundColor: Colors.blueAccent,
-            textStyle: TextStyle(fontSize: 18),
+            foregroundColor: Colors.white, // Цвет текста кнопок
+            backgroundColor: Colors.blueAccent, // Цвет фона кнопок
+            textStyle: TextStyle(fontSize: 18), // Стиль текста кнопок
           ),
         ),
       ),
-      initialRoute: '/',
+      initialRoute: '/', // Начальный маршрут приложения
       routes: {
-        '/': (context) => HomeScreen(),
-        '/screen1': (context) => Screen1(),
-        '/screen2': (context) => Screen2(),
-        '/screen3': (context) => Screen3(),
-        '/screen4': (context) => Screen4(),
-        '/screen5': (context) => Screen5(),
+        '/': (context) => HomeScreen(), // Маршрут для главной страницы
+        '/screen1': (context) => Screen1(), // Маршрут для страницы 1
+        '/screen2': (context) => Screen2(), // Маршрут для страницы 2
+        '/screen3': (context) => Screen3(), // Маршрут для страницы 3
+        '/screen4': (context) => Screen4(), // Маршрут для страницы 4
+        '/screen5': (context) => Screen5(), // Маршрут для страницы 5
       },
     );
   }
 }
 
+// Главная страница
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Главная страница'),
+        title: Text('Главная страница'), // Заголовок AppBar
       ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white, // Фоновый цвет главной страницы
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -84,15 +87,16 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+// Классы для остальных экранов имеют похожую структуру
 class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Страница 1'),
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.yellow, // Цвет AppBar страницы 1
       ),
-      backgroundColor: Colors.yellowAccent,
+      backgroundColor: Colors.yellowAccent, // Фоновый цвет страницы 1
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -111,9 +115,9 @@ class Screen2 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Страница 2'),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.green, // Цвет AppBar страницы 2
       ),
-      backgroundColor: Colors.greenAccent,
+      backgroundColor: Colors.greenAccent, // Фоновый цвет страницы 2
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -132,9 +136,9 @@ class Screen3 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Страница 3'),
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.orange, // Цвет AppBar страницы 3
       ),
-      backgroundColor: Colors.orangeAccent,
+      backgroundColor: Colors.orangeAccent, // Фоновый цвет страницы 3
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -153,9 +157,9 @@ class Screen4 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Страница 4'),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red, // Цвет AppBar страницы 4
       ),
-      backgroundColor: Colors.redAccent,
+      backgroundColor: Colors.redAccent, // Фоновый цвет страницы 4
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -174,9 +178,9 @@ class Screen5 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Страница 5'),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.purple, // Цвет AppBar страницы 5
       ),
-      backgroundColor: Colors.purpleAccent,
+      backgroundColor: Colors.purpleAccent, // Фоновый цвет страницы 5
       body: Center(
         child: ElevatedButton(
           onPressed: () {
